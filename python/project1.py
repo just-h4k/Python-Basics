94bbeada9389f9c4159a5c3990fc49d68de2caad
just-h4k <just_h4k@proton.me>
2026-09-18 04:17:59 +0300

    Add files via upload

---
+++ python/project1.py
@@ -0,0 +1,4 @@
+print ("welcome to aoly's tool to make your channel name")
+nickname = input("what is your naickname?\n")
+user_job = input ("waht is your channel about?\n")
+print ("your channle name could be:"+ user_job + " with " + nickname )
---
+++ python/project2.py
@@ -0,0 +1,2 @@
+a = input("Enter your PIN\n")
+print ("The number of digits in your PIN is:", len(a))
